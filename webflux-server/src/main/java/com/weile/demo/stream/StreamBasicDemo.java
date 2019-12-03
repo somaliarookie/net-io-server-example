141c3e38a8d0f30278bcd90b16e79d78d7609c6c
xubo@akulaku.com <xubo@akulaku.com>
2019-12-03 12:34:27 +0800

    stream basic demo

---
+++ webflux-server/src/main/java/com/weile/demo/stream/StreamBasicDemo.java
@@ -0,0 +1,25 @@
+package com.weile.demo.stream;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @Auth weile
+ * @Time 2019/12/3 12:08
+ * @Description TODO
+ **/
+public class StreamBasicDemo {
+
+
+	public static void main(String[] args) {
+
+		Stream.of("a", "b", "c").forEach(elment -> System.out.println("element:"+elment));
+
+
+		//map reduce
+		IntStream.range(0, 10).map(elment -> elment + 10).filter(e ->  e>15 ).forEach(e-> System.out.println(e));
+		System.out.println(IntStream.range(0, 10).map(elment -> elment + 10).filter(e ->  e>15 ).reduce( (e1,e2)  -> e1+e2));
+
+
+	}
+}
