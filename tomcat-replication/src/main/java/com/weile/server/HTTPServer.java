3a1120da2df1c401ac0ebc534d787cb5cbd35af8
xubo@akulaku.com <xubo@akulaku.com>
2020-06-24 19:06:25 +0800

    tomcat-replication first commit

---
+++ tomcat-replication/src/main/java/com/weile/server/HTTPServer.java
@@ -0,0 +1,60 @@
+package com.weile.server;
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @Auth weile
+ * @Time 2020/6/23 17:33
+ * @Description http 服务器
+ **/
+
+public class HTTPServer {
+
+    public static void main(String[] args) {
+
+        startServer();
+
+    }
+
+    private static void startServer() {
+
+        try {
+            ServerSocket ss = new ServerSocket(8888);
+            System.out.println("启动服务器....");
+
+            while (true) {
+
+                Socket socket = ss.accept();
+//                serveClientWithOneThread(socket);
+                serveClientWithMutipleThread(socket);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void serveClientWithMutipleThread(Socket socket) {
+        new Thread(new Worker(socket),"http-server-worker").start();
+    }
+
+    private static void serveClientWithOneThread(ServerSocket ss) throws IOException {
+        Socket s = ss.accept();
+        System.out.println("客户端:" + s.getRemoteSocketAddress().toString() + "已连接到服务器");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        //读取客户端发送来的消息
+        String mess = br.readLine();
+        System.out.println("客户端：" + mess);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+        bw.write("HTTP/1.1 200 OK" + "\r\n");
+        bw.flush();
+        s.close();
+    }
+
+}
---
+++ tomcat-replication/src/main/java/com/weile/server/Worker.java
@@ -0,0 +1,46 @@
+package com.weile.server;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * @Auth weile
+ * @Time 2020/6/23 19:22
+ * @Description TODO
+ **/
+public class Worker implements Runnable {
+
+    private final Socket socket;
+
+    public Worker(Socket socket) {
+
+        this.socket = socket;
+    }
+
+    public void run() {
+
+        System.out.println("客户端:" + socket.getRemoteSocketAddress().toString() + "已连接到服务器");
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //读取客户端发送来的消息
+            String msg = br.readLine();
+            System.out.println("客户端：" + msg);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bw.write("HTTP/1.1 200 \r\n");
+            bw.write("Server: nginx/1.15.6\r\n");
+            bw.write("Date: Wed, 24 Jun 2020 07:55:00 GMT\r\n");
+            bw.write("Content-Type: application/json\r\n");
+            bw.write("Last-Modified: Wed, 5 August 1996 15:55:28 GMT" + "\r\n");
+            bw.write("Server: Apache 0.84\r\n");
+            bw.write("{\"data\":123}\r\n");
+
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
